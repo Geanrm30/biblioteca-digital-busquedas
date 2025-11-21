5c018d420f56ea0294f65151b1a938372219be95
ronaldo2310 <drromero@uamv.edu.ni>
2025-11-20 19:45:39 -0600

    Implementadas funciones LibroMasReciente y LibroMasAntiguo

---
+++ biblioteca-digital-busquedas/BusquedasFecha.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace biblioteca_digital_busquedas
+{
+    public static class BusquedasFecha
+    {
+        public static Libro LibroMasReciente(List<Libro> listaLibros)
+        {
+            if (listaLibros == null ||  listaLibros.Count == 0) return null;
+
+            Libro reciente = listaLibros[0];
+            foreach (var libro in listaLibros)
+            {
+                if (libro.Anio > reciente.Anio)
+                    reciente = libro;
+            }
+            return reciente;
+        }
+
+        public static Libro LibroMasAntiguo(List<Libro> listaLibros)
+        {
+            if (listaLibros == null ||  listaLibros.Count == 0) return null;
+
+            Libro antiguo = listaLibros[0];
+            foreach (var libro in listaLibros)
+            {
+                if (libro.Anio < antiguo.Anio)
+                    antiguo = libro;
+            }
+            return antiguo;
+        }
+    }
+}
