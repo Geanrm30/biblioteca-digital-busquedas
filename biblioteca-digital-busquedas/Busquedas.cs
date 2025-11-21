ccca3a277bd83b4a68c2fe0513f4c3812363d954
Geanrm30 <garodriguezm@uamv.edu.ni>
2025-11-20 18:54:35 -0600

    Implementada búsqueda lineal por título

---
+++ biblioteca-digital-busquedas/Busquedas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblioteca_digital_busquedas
+{
+    public static class Busquedas
+    {
+        public static Libro BuscarLibroLineal(List<Libro> libros, string tituloBuscado)
+        {
+            foreach (var libro in libros)
+            {
+                if (libro.Titulo.ToLower() == tituloBuscado.ToLower())
+                {
+                    return libro;
+                }
+            }
+
+            return null; // No encontrado
+        }
+    }
+}
