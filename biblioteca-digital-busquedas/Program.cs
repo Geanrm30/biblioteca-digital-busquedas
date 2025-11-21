fd36eaf8968f3cc029c3585d3f63ba147dca30f7
Geanrm30 <garodriguezm@uamv.edu.ni>
2025-11-20 18:53:12 -0600

    Agregada lista de libros para pruebas

--- biblioteca-digital-busquedas/Program.cs
+++ biblioteca-digital-busquedas/Program.cs
@@ -8,8 +8,16 @@ namespace biblioteca_digital_busquedas
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            List<Libro> libros = new List<Libro>
+            {
+                new Libro{ Id=1, Titulo="Programación en C#", Autor="Alvarez", Anio=2020, Descripcion="Guía completa de C#." },
+                new Libro{ Id=2, Titulo="Estructuras de datos", Autor="Martínez", Anio=2018, Descripcion="Incluye pilas, colas y árboles." },
+                new Libro{ Id=3, Titulo="Inteligencia Artificial", Autor="Soto", Anio=2022, Descripcion="Conceptos modernos de IA." },
+                new Libro{ Id=4, Titulo="Bases de Datos", Autor="López", Anio=2019, Descripcion="Normalización y SQL." },
+            };
         }
     }
 }
